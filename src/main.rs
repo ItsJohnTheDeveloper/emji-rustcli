b2490b7f70e775226322feff2faf350d638f9c30
Jonathan Derbyshire <jmderby@hotmail.com>
2022-12-28 12:38:02 -0800

    improved var naming

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use serde::Deserialize;
 #[derive(Parser)]
 struct Cli {
     // the pattern to look for
-    fruit: String,
+    food: String,
     qty: i32,
 }
 
@@ -24,18 +24,18 @@ struct Emoji {
 fn get_food_emoji(input: &str) -> Option<String> {
     let json_file = fs::read_to_string("src/data/foods.json").expect("Unable to read file");
     let foods: Emojis = serde_json::from_str(&json_file).expect("Unable to parse JSON");
-    let fruit_emojis = &foods.emojis;
+    let food_emojis = &foods.emojis;
 
-    // Return explicit fruit match from input
-    for fruit in fruit_emojis {
-        if fruit.name == input {
-            return Some(fruit.emoji.to_string());
+    // Return explicit food match from input
+    for food in food_emojis {
+        if food.name == input {
+            return Some(food.emoji.to_string());
         }
     }
-    // Return loose fruit match from input
-    for fruit in fruit_emojis {
-        if fruit.name.contains(input) {
-            return Some(fruit.emoji.to_string());
+    // Return loose food match from input
+    for food in food_emojis {
+        if food.name.contains(input) {
+            return Some(food.emoji.to_string());
         }
     }
     None
@@ -44,16 +44,16 @@ fn get_food_emoji(input: &str) -> Option<String> {
 fn main() {
     let args = Cli::parse();
 
-    let fruit_selected = &get_food_emoji(&args.fruit);
+    let food_selected = &get_food_emoji(&args.food);
     let qty_selected = args.qty.to_string().parse::<i32>().unwrap();
     
-    match fruit_selected {
+    match food_selected {
         Some(val) => {
             for _ in 0..qty_selected {
                 print!("{val}");
             }
             println!("");
         },
-        None => println!("Unknown fruit :(")
+        None => println!("Unknown food :(")
     }
 }
